d4077f34e5b53c2652880c03c2248b2227aaee43
elena-dragunova <33746898+elena-dragunova@users.noreply.github.com>
2020-08-13 17:25:34 +0500

    Add redux to Quiz component.

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,9 +1,22 @@
-import {FETCH_QUIZZES_ERROR, FETCH_QUIZZES_START, FETCH_QUIZZES_SUCCESS} from '../actions/actionTypes';
+import {
+  FETCH_QUIZ_ERROR,
+  FETCH_QUIZ_SUCCESS,
+  FETCH_QUIZZES_ERROR,
+  FETCH_QUIZZES_START,
+  FETCH_QUIZZES_SUCCESS, FINISH_QUIZ, QUIZ_NEXT_QUESTION,
+  QUIZ_SET_STATE,
+  RETRY_QUIZ,
+} from '../actions/actionTypes';
 
 const initialState = {
   quizzes: [],
   loading: false,
   error: null,
+  results: {},
+  isFinished: false,
+  activeQuestion: 0,
+  answerState: null,
+  quiz: null,
 };
 
 export default function quizReducer(state = initialState, action) {
@@ -25,8 +38,43 @@ export default function quizReducer(state = initialState, action) {
         loading: false,
         error: action.err,
       };
-
-
+    case FETCH_QUIZ_SUCCESS:
+      return {
+        ...state,
+        quiz: action.quiz,
+        loading: false,
+      };
+    case FETCH_QUIZ_ERROR:
+      return {
+        ...state,
+        error: action.err,
+        loading: false,
+      };
+    case QUIZ_SET_STATE:
+      return {
+        ...state,
+        answerState: action.answerState,
+        results: action.results,
+      };
+    case FINISH_QUIZ:
+      return {
+        ...state,
+        isFinished: true,
+      };
+    case QUIZ_NEXT_QUESTION:
+      return {
+        ...state,
+        answerState: null,
+        activeQuestion: action.number,
+      };
+    case RETRY_QUIZ:
+      return {
+        ...state,
+        results: {},
+        isFinished: false,
+        activeQuestion: 0,
+        answerState: null,
+      };
     default:
       return state;
   }
