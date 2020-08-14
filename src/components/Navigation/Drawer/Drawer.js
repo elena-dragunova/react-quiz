c5e3a51a88c04a67ed5cab62cb083741added502
elena-dragunova <33746898+elena-dragunova@users.noreply.github.com>
2020-08-14 17:05:33 +0500

    Add authorization.

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -3,31 +3,13 @@ import styles from './Drawer.module.css'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 import { NavLink } from 'react-router-dom'
 
-const links = [
-  {
-    to: '/',
-    label: 'List of Quizzes',
-    exact: true,
-  },
-  {
-    to: '/auth',
-    label: 'Authorization',
-    exact: false,
-  },
-  {
-    to: '/quiz-creator',
-    label: 'Create quiz',
-    exact: false,
-  },
-];
-
 class Drawer extends Component {
 
   clickHandler = () => {
     this.props.onClose();
   };
 
-  renderLinks() {
+  renderLinks(links) {
     return links.map((link, index) => {
       return (
         <li
@@ -53,11 +35,39 @@ class Drawer extends Component {
       classes.push(styles.close);
     }
 
+    const links = [
+      {
+        to: '/',
+        label: 'List of Quizzes',
+        exact: true,
+      },
+
+    ];
+
+    if (this.props.isAuthenticated) {
+      links.push({
+        to: '/quiz-creator',
+        label: 'Create quiz',
+        exact: false,
+      });
+      links.push({
+        to: '/logout',
+        label: 'Logout',
+        exact: false,
+      })
+    } else {
+      links.push({
+        to: '/auth',
+        label: 'Authorization',
+        exact: false,
+      },)
+    }
+
     return (
       <React.Fragment>
         <nav className={classes.join(' ')}>
           <ul>
-            {this.renderLinks()}
+            {this.renderLinks(links)}
           </ul>
         </nav>
         { this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null }
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import quizReducer from './quiz';
 import createReaducer from './create';
+import authReducer from './auth';
 
 export default combineReducers({
   quiz: quizReducer,
-  create: createReaducer
+  create: createReaducer,
+  auth: authReducer,
 });
